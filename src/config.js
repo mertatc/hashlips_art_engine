6a627ebab69566e1ff5bae1b7809ccbd1738f815
mertatc <mertatc1903@hotmail.com>
2022-07-28 13:47:25 +0300

    first

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
+const namePrefix = "Kripto Türkiye";
+const description = "Kripto Türkiye Stickers";
 const baseUri = "ipfs://NewUriToReplace";
 
 const solanaMetadata = {
@@ -24,15 +24,12 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 10,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Left Eye" },
+      { name: "Right Eye" },
+      { name: "Mouth" },
     ],
   },
 ];
